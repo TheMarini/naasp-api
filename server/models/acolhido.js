7667486fc5f62906e98756bf6b2839098d43cefe
Pedro <pedro.guerra@tcsindustrial.com.br>
2020-04-02 16:00:34 -0300

    add rotas get, get?id= ,getFull, delete

--- server/models/acolhido.js
+++ server/models/acolhido.js
@@ -22,8 +22,8 @@ module.exports = (sequelize, DataTypes) => {
     localNascimento: DataTypes.STRING,
     encaminhamento: DataTypes.STRING,
     observacao: DataTypes.STRING,
-    pessoaId: DataTypes.INTEGER,
-    religiaoId: DataTypes.INTEGER,
+    PessoaId: DataTypes.INTEGER,
+    ReligiaoId: DataTypes.INTEGER,
     updatedAt: DataTypes.DATE,
     createdAt: DataTypes.DATE
   }, {
@@ -32,12 +32,14 @@ module.exports = (sequelize, DataTypes) => {
 
   Acolhido.associate = function (models) {
     Acolhido.belongsTo(models.Pessoa, {
-      foreignKey: 'pessoaId'
+      foreignKey: 'PessoaId'
     });
     Acolhido.belongsTo(models.Religiao, {
-      foreignKey: 'religiaoId'
+      foreignKey: 'ReligiaoId'
     });
     Acolhido.hasMany(models.Familiar)
+    Acolhido.hasMany(models.DoencaFamilia)
+    Acolhido.hasMany(models.MedicamentoContinuo)
   };
 
   Acolhido.adiciona = async function (models, param) {
@@ -83,15 +85,78 @@ module.exports = (sequelize, DataTypes) => {
         demanda: acolhidoParam.demanda,
         encaminhamento: acolhidoParam.encaminhamento,
         observacao: acolhidoParam.observacao,
-        pessoaId: pessoaInstance.dataValues.id,
-        religiaoId: religiaoInstance[0].dataValues.id
+        PessoaId: pessoaInstance.dataValues.id,
+        ReligiaoId: religiaoInstance[0].dataValues.id
       }, {
         queryOptions
       })
-      let acolhidoId = acolhidoInstance.dataValues.id
-      let medicamentoContinuo = await models.MedicamentoContinuo.adicionaVarios(medicamentos, acolhidoId)
-      let familiaresInstance = await models.Familiar.adicionaVarios(familiares, acolhidoId)
-      // await models.DoencaFamilia.adicionaVarios(doencaFamilia, acolhidoId)
+      let AcolhidoId = acolhidoInstance.dataValues.id
+      let medicamentoContinuo = await models.MedicamentoContinuo.adicionaVarios(medicamentos, AcolhidoId)
+      let familiaresInstance = await models.Familiar.adicionaVarios(familiares, AcolhidoId)
+      // await models.DoencaFamilia.adicionaVarios(doencaFamilia, AcolhidoId)
+      // await transaction.commit()
+
+      return acolhidoInstance
+    } catch (error) {
+      // await transaction.rollback();
+      console.log("\n", error, "\n")
+      throw util.checkError(error, modelName)
+    }
+
+  }
+
+  Acolhido.edita = async function (models, param) {
+    let {
+      endereco,
+      cidade,
+      bairro,
+      pessoa,
+      religiao,
+      acolhidoParam,
+      familiares = [],
+      medicamentos = [],
+      doencaFamilia = [],
+    } = param
+
+    let queryOptions = {}
+    let transaction = null
+
+    try {
+      //religiaoInstance - array
+      let religiaoInstance = await models.Religiao.pesquisaOuAdiciona(religiao)
+      let pessoaInstance = await models.Pessoa.edita(models, transaction, {
+        pessoa,
+        endereco,
+        cidade,
+        bairro
+      })
+      // console.log("RELIGIAO datavalues", religiaoInstance[0].)
+      queryOptions.transaction = transaction
+
+      if (!religiaoInstance || !pessoaInstance) {
+        // transaction.rollback();
+        return util.defineError(412, "Erro em religião")
+      }
+
+      let acolhidoInstance = await Acolhido.update({
+        atividade_fisica: acolhidoParam.atividade_fisica,
+        bebida_quantidade: acolhidoParam.bebida_quantidade,
+        bebida_periodicidade: acolhidoParam.bebida_periodicidade,
+        paroquia: acolhidoParam.paroquia,
+        atividades_religiosas: acolhidoParam.atividades_religiosas,
+        localNascimento: acolhidoParam.localNascimento,
+        demanda: acolhidoParam.demanda,
+        encaminhamento: acolhidoParam.encaminhamento,
+        observacao: acolhidoParam.observacao,
+        PessoaId: pessoaInstance.dataValues.id,
+        ReligiaoId: religiaoInstance[0].dataValues.id
+      }, {
+        queryOptions
+      })
+      let AcolhidoId = acolhidoInstance.dataValues.id
+      let medicamentoContinuo = await models.MedicamentoContinuo.adicionaVarios(medicamentos, AcolhidoId)
+      let familiaresInstance = await models.Familiar.adicionaVarios(familiares, AcolhidoId)
+      // await models.DoencaFamilia.adicionaVarios(doencaFamilia, AcolhidoId)
       // await transaction.commit()
 
       return acolhidoInstance
@@ -113,6 +178,60 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
 
+  Acolhido.pesquisaAcolhidoCompleto = async function (models, id) {
+    let {
+      Religiao,
+      Pessoa,
+      Endereco,
+      Cidade,
+      Bairro,
+      Familiar,
+      DoencaFamilia,
+      MedicamentoContinuo
+    } = models
+
+    try {
+      let acolhidoInstance = await Acolhido.findByPk(id, {
+        include: [{
+            model: Religiao,
+            attributes: ['nome'],
+            as: 'Religiao'
+          },
+          {
+            model: Pessoa,
+            as: 'Pessoa',
+            include: [{
+              model: Endereco,
+              as: 'Endereco',
+              include: [{
+                model: Cidade,
+                attributes: ['nome'],
+                as: 'Cidade'
+              }, {
+                model: Bairro,
+                attributes: ['nome'],
+                as: 'Bairro'
+              }]
+            }]
+          }, {
+            model: Familiar,
+            as: 'Familiars'
+          }, {
+            model: DoencaFamilia,
+            as: 'DoencaFamilia'
+          }, {
+            model: MedicamentoContinuo,
+            as: 'MedicamentoContinuos'
+          }
+        ]
+      })
+      return acolhidoInstance
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+
   Acolhido.lista = async function (id) {
     try {
       let acolhidoInstance = await Acolhido.findAll()
@@ -122,6 +241,26 @@ module.exports = (sequelize, DataTypes) => {
       throw util.checkError(error, modelName)
     }
   }
+
+  Acolhido.deleta = async function (idParam, transaction) {
+    let queryOptions = {
+      where: {
+        id: idParam
+      }
+    }
+    
+    if (transaction)
+      queryOptions.transaction = transaction
+
+    try {
+      let acolhido = await Acolhido.destroy(queryOptions)
+      return acolhido
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+
 
   return Acolhido;
 };
