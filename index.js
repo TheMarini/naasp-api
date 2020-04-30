216b187380f1809ca514a9323b0b76819c927c0e
guigawiller <guilhermewiller@hotmail.com>
2020-04-30 17:18:08 -0300

    [~] Corrige host e porta do postgress no index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
+    port:     '5432',
+    host:     'ec2-34-234-228-127.compute-1.amazonaws.com',
     logging:  true //false
   })
   sequelize.sync({ force: true }).then(() => {
