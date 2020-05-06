a46567202a0965f37270681345c7509748d89be3
Pedro <pedro.guerra@tcsindustrial.com.br>
2020-05-06 12:14:14 -0300

    fix rotas get Acolhido/Voluntario

--- server/controllers/voluntarioController.js
+++ server/controllers/voluntarioController.js
@@ -21,26 +21,13 @@ exports.post = async function (req, res) {
 
 }
 
-exports.getFull = async function (req, res) {
-	let response = null
-	try {
-		response = await models.Voluntario.pesquisaVoluntarioCompleto(models, req.query.id)
-
-		res.status(successStatus).json(response)
-	} catch (error) {
-		console.log("\n", error, "\n")
-		res.status(error.code).json(error.message)
-	}
-
-}
-
 exports.get = async function (req, res) {
 	let response = null
 	try {
 		if (req.query.id)
-			response = await models.Voluntario.pesquisa(req.query.id)
+			response = await models.Voluntario.pesquisa(models, req.query.id)
 		else
-			response = await models.Voluntario.lista()
+			response = await models.Voluntario.lista(models)
 
 		res.status(successStatus).json(response)
 	} catch (error) {
