7bc7cb5e98ab6fd8553c4fca7da2ce9e166486db
pedroguerra <pedroguerra@pop-os.localdomain>
2020-06-03 11:03:59 -0300

    validacao

--- server/migrations/20200219230038-create-acolhido.js
+++ server/migrations/20200219230038-create-acolhido.js
@@ -32,6 +32,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       preferenciaAtendimento: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       observacao: {
