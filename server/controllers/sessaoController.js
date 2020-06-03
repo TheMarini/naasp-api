1a0cb36e3946579e58f60d1be7c8cdc5c7377a1c
guigawiller <guilhermewiller@hotmail.com>
2020-06-03 05:49:25 -0300

    [+] Finaliza BackEnd agenda

--- server/controllers/sessaoController.js
+++ server/controllers/sessaoController.js
@@ -2,77 +2,97 @@ const models = require('../models')
 
 var successStatus = 200
 
-exports.post = async function (req, res) {
+exports.post = async function (req) {
 	let response = null
 	try {
-		response = await models.Voluntario.adiciona(models, {
-			endereco: req.body.endereco,
-			cidade: req.body.cidade,
-			bairro: req.body.bairro,
-			pessoa: req.body.pessoa,
-			especialidade: req.body.especialidade
-		})
-		res.status(successStatus).json(response)
+		response = await models.Sessao.adiciona(models, {
+			dataSessao: req.dataSessao,
+			horaSessao: req.horaSessao,
+			acolhidoId: req.acolhidoId,
+			voluntarioId: req.voluntarioId,
+			presenca: req.presenca,
+			observacao: req.observacao
+				})
+		// res.status(successStatus).json(response)
 	} catch (error) {
 		console.log("\n", error, "\n")
-		res.status(error.code).json(error.message)
+		// res.status(error.code).json(error.message)
 	}
 
 }
-
-exports.getFull = async function (req, res) {
+exports.get = async function (req) {
 	let response = null
 	try {
-		response = await models.Voluntario.pesquisaVoluntarioCompleto(models, req.query.id)
-
-		res.status(successStatus).json(response)
+		// if (req.id)
+		// 	response = await models.Sessao.pesquisa(req.id)
+		// else
+			response = await models.Sessao.lista()
+			// console.log(response);
+			return response;
+		// res.status(successStatus).json(response)
 	} catch (error) {
 		console.log("\n", error, "\n")
-		res.status(error.code).json(error.message)
-	}
-
-}
-
-exports.get = async function (req, res) {
-	let response = null
-	try {
-		if (req.query.id)
-			response = await models.Voluntario.pesquisa(req.query.id)
-		else
-			response = await models.Voluntario.lista()
-
-		res.status(successStatus).json(response)
-	} catch (error) {
-		console.log("\n", error, "\n")
-		res.status(error.code).json(error.message)
+		// res.status(error.code).json(error.message)
 	}
 }
 
-exports.put = async function (req, res) {
-	let response = null
-	try {
-		response = await models.Voluntario.edita(models, {
-			endereco: req.body.endereco,
-			cidade: req.body.cidade,
-			bairro: req.body.bairro,
-			pessoa: req.body.pessoa,
-			especialidade: req.body.especialidade,
-			voluntarioId: req.body.voluntarioId
-		})
 
-		res.status(successStatus).json(response)
-	} catch (error) {
-		console.log("\n", error, "\n")
-		res.status(error.code).json(error.message)
-	}
-}
 
-exports.delete = async function (req, res) {
-	try {
-		let response = await models.Voluntario.deleta(req.query.id)
-		res.status(successStatus).json(response)
-	} catch (error) {
-		console.log("\n", error, "\n")
-		res.status(error.code).json(error.message)
-	}
-}
+
+//
+// exports.getFull = async function (req, res) {
+// 	let response = null
+// 	try {
+// 		response = await models.Voluntario.pesquisaVoluntarioCompleto(models, req.query.id)
+//
+// 		res.status(successStatus).json(response)
+// 	} catch (error) {
+// 		console.log("\n", error, "\n")
+// 		res.status(error.code).json(error.message)
+// 	}
+//
+// }
+//
+// exports.get = async function (req, res) {
+// 	let response = null
+// 	try {
+// 		if (req.query.id)
+// 			response = await models.Voluntario.pesquisa(req.query.id)
+// 		else
+// 			response = await models.Voluntario.lista()
+//
+// 		res.status(successStatus).json(response)
+// 	} catch (error) {
+// 		console.log("\n", error, "\n")
+// 		res.status(error.code).json(error.message)
+// 	}
+// }
+//
+// exports.put = async function (req, res) {
+// 	let response = null
+// 	try {
+// 		response = await models.Voluntario.edita(models, {
+// 			endereco: req.body.endereco,
+// 			cidade: req.body.cidade,
+// 			bairro: req.body.bairro,
+// 			pessoa: req.body.pessoa,
+// 			especialidade: req.body.especialidade,
+// 			voluntarioId: req.body.voluntarioId
+// 		})
+//
+// 		res.status(successStatus).json(response)
+// 	} catch (error) {
+// 		console.log("\n", error, "\n")
+// 		res.status(error.code).json(error.message)
+// 	}
+// }
+//
+// exports.delete = async function (req, res) {
+// 	try {
+// 		let response = await models.Voluntario.deleta(req.query.id)
+// 		res.status(successStatus).json(response)
+// 	} catch (error) {
+// 		console.log("\n", error, "\n")
+// 		res.status(error.code).json(error.message)
+// 	}
+// }
