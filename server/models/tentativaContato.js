088d2b406780e3017bdf3370cb62449c52cf09c2
Pedro <pedrowar.pw@gmail.com>
2020-06-09 22:23:27 -0300

    finalizar model + iniciar hook

--- server/models/tentativaContato.js
+++ server/models/tentativaContato.js
@@ -1,4 +1,6 @@
 'use strict';
+const util = require("../util")
+const modelName = "TentativaContato"
 module.exports = (sequelize, DataTypes) => {
   const TentativaContato = sequelize.define('TentativaContato', {
     id: {
@@ -8,11 +10,19 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true
     },
     presenca: DataTypes.BOOLEAN,
+    horario: DataTypes.DATE,
     AcolhidoId: DataTypes.INTEGER,
     UsuarioId: DataTypes.INTEGER,
     createdAt: DataTypes.DATE,
     updatedAt: DataTypes.DATE
-  }, {});
+  }, {
+    hooks: {
+      afterCreate: (param1, param2, param3) => {
+        console.log(param1, param2, param3)
+      }
+    }
+  })
+
   TentativaContato.associate = function(models) {
     TentativaContato.belongsTo(models.Usuario, {
       foreignKey: 'UsuarioId'
@@ -22,17 +32,27 @@ module.exports = (sequelize, DataTypes) => {
     })
   }
 
-  TentativaContato.adiciona = async function (presencaParam, t) {
-    
+  TentativaContato.adiciona = async function (models, param) {
+    console.log(param)
+    let t = await sequelize.transaction({type: sequelize.Transaction})
     let queryOptions = {}
+    let {
+      Usuario,
+      Acolhido
+    } = models
 
-    if (transaction)
+    if (t)
       queryOptions.transaction = t
 
     try {
       let tentativaContatoInstance = await TentativaContato.create({
-        presenca: presencaParam
+        presenca: param.presenca,
+        horario: param.horario  
       }, queryOptions)
+
+      await TentativaContato.atualizaUsuario(Usuario, param, tentativaContatoInstance, t)
+      await TentativaContato.atualizaAcolhido(Acolhido, param, tentativaContatoInstance, t)
+
       return tentativaContatoInstance
     } catch (error) {
       console.log("\n catch \n")
@@ -40,5 +60,216 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
 
-  return TentativaContato;
-};
+  TentativaContato.edita = async function (models, presencaParam) {
+    let t = await sequelize.transaction({type: sequelize.Transaction})
+
+    let queryOptions = {
+      where: {
+        id: presencaParam.id
+      }
+    }
+    let {
+      Usuario,
+      Acolhido
+    } = models
+
+    if (t)
+      queryOptions.transaction = t
+
+    try {
+      let tentativaContatoInstance = await TentativaContato.create({
+        presenca: presencaParam,
+        horario: horarioParam
+      }, queryOptions)
+
+      await TentativaContato.atualizaUsuario(Usuario, param, tentativaContatoInstance, t)
+      await TentativaContato.atualizaAcolhido(Acolhido, param, tentativaContatoInstance, t)
+
+      return tentativaContatoInstance
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+  
+  TentativaContato.pesquisa = async function(models, id) {
+    let {
+      Usuario,
+      Voluntario,
+      Acolhido
+    } = models
+
+    try {
+      let tentativaContatoInstance = await TentativaContato.findByPk(id, {
+        include: [{
+          model: Usuario,
+          as: "Usuario",
+          include: [{
+            model: Voluntario,
+            as: "Voluntario",
+            include: { all: true }
+          }]
+        },
+        {
+          model: Acolhido,
+          as: "Acolhido",
+          include: { all: true }
+        }]
+      })
+      return tentativaContatoInstance
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+
+  TentativaContato.lista = async function(models) {
+    let {
+      Usuario,
+      Voluntario,
+      Acolhido
+    } = models
+
+    try {
+      let tentativaContatoInstance = await TentativaContato.findAll({
+        include: [{
+          model: Usuario,
+          as: "Usuario",
+          include: [{
+            model: Voluntario,
+            as: "Voluntario",
+            include: { all: true }
+          }]
+        },
+        {
+          model: Acolhido,
+          as: "Acolhido",
+          include: { all: true }
+        }]
+      })
+      return tentativaContatoInstance
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+
+  TentativaContato.listaPorAcolhido = async function(models, acolhidoId) {
+    let {
+      Usuario,
+      Voluntario,
+      Acolhido
+    } = models
+
+    try {
+      let tentativaContatoInstance = await TentativaContato.findAll({
+        where: {
+          AcolhidoId: acolhidoId
+        },
+        include: [{
+          model: Usuario,
+          as: "Usuario",
+          include: [{
+            model: Voluntario,
+            as: "Voluntario",
+            include: { all: true }
+          }]
+        },
+        {
+          model: Acolhido,
+          as: "Acolhido",
+          include: { all: true }
+        }]
+      })
+      return tentativaContatoInstance
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+
+  TentativaContato.listaPorUsuario = async function(models, usuarioId) {
+    let {
+      Usuario,
+      Voluntario,
+      Acolhido
+    } = models
+
+    try {
+      let tentativaContatoInstance = await TentativaContato.findAll({
+        where: {
+          UsuarioId: usuarioId
+        },
+        include: [{
+          model: Usuario,
+          as: "Usuario",
+          include: [{
+            model: Voluntario,
+            as: "Voluntario",
+            include: { all: true }
+          }]
+        },
+        {
+          model: Acolhido,
+          as: "Acolhido",
+          include: { all: true }
+        }]
+      })
+      return tentativaContatoInstance
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+
+  TentativaContato.atualizaUsuario = async function (Usuario, param, tentativaContatoInstance, t){
+    let usuarioInstance  = null
+    let { usuarioId } = param
+    let queryOptions = {
+      transaction: t
+    }
+
+    if(usuarioId)
+      usuarioInstance = await Usuario.findByPk(usuarioId, queryOptions)
+    else 
+      throw util.defineError(400, "Usuário não existe")
+    
+    tentativaContatoInstance.setUsuario(usuarioInstance, queryOptions)
+  }
+  
+  TentativaContato.atualizaAcolhido = async function (Acolhido, param, tentativaContatoInstance, t){
+    let acolhidoInstance  = null
+    let { acolhidoId } = param
+    let queryOptions = {
+      transaction: t
+    }
+
+    if(acolhidoId)
+      acolhidoInstance = await Acolhido.findByPk(acolhidoId, queryOptions)
+    else 
+      throw util.defineError(400, "Acolhido não existe")
+    
+    tentativaContatoInstance.setAcolhido(acolhidoInstance, queryOptions)
+  }
+  
+  TentativaContato.deleta = async function (idParam, t) {
+    let queryOptions = {
+      where: {
+        id: idParam
+      }
+    }
+
+    if (t)
+      queryOptions.transaction = t
+
+    try {
+      await TentativaContato.destroy(queryOptions)
+      return true
+    } catch (error) {
+      console.log("\n catch \n")
+      throw util.checkError(error, modelName)
+    }
+  }
+
+  return TentativaContato
+}
